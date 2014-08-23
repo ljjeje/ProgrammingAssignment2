73307884afe87b4dac9a45ad9bbd6c4286167b5f
Evangeline Johnston <ljjeje@gmail.com>
2014-08-23 01:54:13 -0700

    Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function makes a place to store all the information we wish to save
+## or not recalculate in the future. The second inputs the values into the space 
+##the first function created. Please use getsolve and setsolve to interact with
+## the function.
 
-## Write a short comment describing this function
+## Makes a cache for the information about the matrix so that information can 
+##be retained.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(inverse) m <<- inverse
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This solves for the inverse of the square matrix and saves the information into
+##the space created for it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setsolve(m)
 }
+##Hope you enjoyed it.
